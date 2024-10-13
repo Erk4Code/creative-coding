4adcfc00e8178d2f54fc642fbdad077267a77845
Eric Bae <ebae1701@gmail.com>
2024-10-12 22:26:47 -0400

    Finished grid assignment added framerate animation

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -1,19 +1,27 @@
 function setup() {
   // create a canvas
   createCanvas(1200, 1200);
-
   // disable animation
-  noLoop();
+  frameRate(10);
+  //noLoop();
+  
 }
 
 function draw() {
   background(220);
-  translate(100, 100);
-  for(let x = 0; x < 8; x++){
-    for(let y = 0; y < 8; y++){
+  translate(70, 70);
+  noStroke();
+
+  for(let x = 0; x < 10; x++){
+    
+    for(let y = 0; y < 10; y++){
       push();
-      translate(x * 135, y * 135);
-      ellipse(0,0,100,100);
+      let a = random(10, 99);
+      let b = random(127,190);
+
+      translate(x * 110, y * 110);
+      fill(b);
+      square(0,0,a); 
       pop();
       
     }
