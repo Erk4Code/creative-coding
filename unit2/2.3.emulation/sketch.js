56dc439211e8e6a6be2a097e3462f2b08a62c4e6
Eric Bae <ebae1701@gmail.com>
2024-10-17 02:38:36 -0400

    Used random positioning for canvas
    Completed functions for black and red boxes
    
    used random seed to position every box randomly
    
    2 for loops being used

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -1,6 +1,7 @@
 function setup() {
-  //for my project I decided to create the exact replica of https://spalterdigital.com/artworks/walk-through-raster-series-7-3-3-1/
+  //for my project I decided to create something similar of https://spalterdigital.com/artworks/walk-through-raster-series-7-3-3-1/
   //for this I will try to implement functions to draw the different black and red squares and then add position parameters to draw them accordingly on the canvas.
+  //it will use a random function to position all the squares randomly along the canvas
 
 
   // create the canvas
@@ -20,12 +21,51 @@ function drawBlackBox(x, y){
 
   rect(x,y,62,60); // fill boarder for the right side
   rect(x,y,58,60);
-
 }
 
-// red color 166 61 53
+function drawRedBox(x, y){
+  noFill(); //sadly you cannot get opaque on boarders only within the shape's color itself
+  strokeWeight(2);
+  stroke('rgb(186,54,36)'); // color of red used in the red boxes
+  square(x,y, 60); //same square size
+  rect(x,y-2,60,62); //flip the values based on the y positioning for top boarder
+  rect(x,y+2,60,58);
+
+  rect(x,y,60,62); // flip values for y for the bottom boarder
+  rect(x,y,60,58);
+}
+
 function draw() {
-  background(247,243,240);
-  drawBlackBox(160, 145);
+  background(237,237,237); // background canvas color
+  randomSeed(99); // to get a consistent seed and same canvas for every refresh
+
+  for(let i = 0; i < 40; i++){ //40 red squares and being drawn first then overwritten by black
+    let x = random(130, 650);
+    let y = random(130, 800);
+    drawRedBox(x,y);
+
+  }
+  for(let i = 0; i < 40; i++){ //40 black squares are being drawn
+    let x = random(130, 650);
+    let y = random(130, 800);
+    drawBlackBox(x,y);
+
+  }
+  // Drawings and locations for all individual black boxes
+  //drawBlackBox(160, 145);
+  //drawBlackBox(160, 178);
+  //drawBlackBox(194, 194);
+  //drawBlackBox(227, 210); // first black chunk to the top left
+  //drawBlackBox(261, 245);
+  //drawBlackBox(261, 163);
+  //drawBlackBox(294, 194);
+
+  //drawBlackBox(140, 296); // single box left
+
+  //drawBlackBox(140, 420);
+  //drawBlackBox(123, 469);
+  //drawBlackBox(123, 469);
+  //decided to keep the above because it was hard work then I went with random positioning instead
+
 
 }
