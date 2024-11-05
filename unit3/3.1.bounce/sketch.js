6d519fae2a01201a5132e0709729993099705839
Eric Bae <ebae1701@gmail.com>
2024-11-05 18:58:51 -0500

    circle bounce
    change the color next

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -1,7 +1,23 @@
+let x = 340;
+let y = 150;
+let xspeed = 4;
+let yspeed = 4;
+let r = 20;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  ellipse(x,y,r*2,r*2);
+  x = x + xspeed; //iterate x
+  y = y + yspeed; //irerate y 
+  if(x > width -r || x<r){
+    xspeed = -xspeed;
+  }
+  if(y > height -r || y <r){
+    yspeed = -yspeed;
+  }
+  
 }
