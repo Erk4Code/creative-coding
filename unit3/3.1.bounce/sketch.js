3b221c2d3d087aff7dd644ae3e32e3450c2a6921
Eric Bae <ebae1701@gmail.com>
2024-11-05 23:56:32 -0500

    added comments and changed speed

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -1,21 +1,22 @@
 let x = 340;
 let y = 150;
-let xspeed = 4;
-let yspeed = 4;
-let r = 20;
-let color;
+let xspeed = 4; //speed of circle traveling
+let yspeed = 6; //6 to vary in the travel
+let r = 20; // radius of the circle
+let color; // change color of circle when bouncing
 
 function setup() {
   createCanvas(400, 400);
-  color = [random(255), random(255), random(255)];
+  color = [random(255), random(255), random(255)]; // random color list
 }
 
 function draw() {
   background(220);
   fill(color);
-  ellipse(x,y,r*2,r*2);
+  ellipse(x,y,r*2,r*2); // drawing the circle
   x = x + xspeed; //iterate x
   y = y + yspeed; //irerate y 
+  
   if(x > width -r || x<r){
     xspeed = -xspeed;
     color = [random(255), random(255), random(255)]; // Change color on x collision
