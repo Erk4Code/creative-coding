d9e8220d87daf6aa06964590f21a3e14f27a9a7c
Eric Bae <ebae1701@gmail.com>
2024-11-11 11:59:24 -0500

    Started on ripple

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,7 +1,23 @@
+let ripple_x = 0;
+let ripple_y = 0;
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background("#FFC0CB");
+
+  push();
+  fill("white");
+  textSize(50);
+  text("ripple_x:" + ripple_x, 50, 50);
+  text("ripple_y:" + ripple_y, 50, 90);
+  pop();
+}
+
+function mousePressed(){
+  ripple_x = mouseX;
+  ripple_y = mouseY;
+
 }
