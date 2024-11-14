7c71b3107d83a4d956730a4639ae9928b20af491
Eric Bae <ebae1701@gmail.com>
2024-11-14 16:03:55 -0500

    Started on House of Dust

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,25 @@
+let quatrain = '';
+
+let grammar = tracery.createGrammar({
+  "material":[
+    "SAND",
+    "PLASTIC",
+    "LEAVES",
+    "WARFRAME"
+  ],
+  "origin": "A HOUSE OF #material#"
+})
+quatrain = grammar.flatten("#origin#");
+
+
 function setup() {
   createCanvas(800, 800);
+  noLoops();
 }
+
 
 function draw() {
   background(220);
+  textSize(40);
+  text(quatrain, 50,50); 
 }
