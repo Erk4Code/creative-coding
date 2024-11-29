c610a74a6a95bd3fa02080004f3bd0e8b1900254
Eric Bae <ebae1701@gmail.com>
2024-11-29 00:15:44 -0500

    Implementing audio visualizer for screen saver
    Added in audio file
    Reference to video
    Testing to see if audio works

--- unit3/3.x.screen-saver/sketch.js
+++ unit3/3.x.screen-saver/sketch.js
@@ -1,7 +1,30 @@
+// for my screen-saver I will be referencing this video https://www.youtube.com/watch?v=uk96O7N1Yo0
+
+// it is a tutorial on creating an audio visualizer I thought that since it had particles that interacted with music it would help "save the screen" from burnout
+
+// I will try to change some implementations that are from the video so I also have my own implementations and not just copied over the code
+
+//audio used from nocopyrightsounds
+var song
+
+function preload(){
+  song = loadSound('TULE, Chris Linton - Fearless pt. II (feat. Chris Linton) [NCS Release].mp3')
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(0);
+}
+
+function mouseClicked(){ //test to see if audio is playing
+  if(song.isPlaying()){
+    song.pause()
+  }else{
+    song.play()
+  }
+
 }
