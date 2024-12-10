b092542ea6601616a35b3819482ff9f4b4f4b9e3
Eric Bae <ebae1701@gmail.com>
2024-12-10 03:19:18 -0500

    Debug if song works and work on speed change and key presses

--- unit5/final/sketch.js
+++ unit5/final/sketch.js
@@ -4,31 +4,62 @@
 let w = 0;
 let h = 0;
 
+let s = 'play';
+let score = 0;
+let gameOver = false;
+let song;
+
 class Tile{ // title constructor to generate tiles on the scroller
   constructor(lane){ // lane parameter lanes are from 0 ,1 ,2 ,3 so in total 4 lanes
     this.lane = lane;
-    this.w = w/4;
+    this.w = w/4 - 20;
     this.h = h/5;
-    this.x = this.lane*w/4;
-    this.y = 0;
+    this.x = 10 + this.lane*w/4;
+    this.y = -2*this.h;
+    this.speed = 15; // going to change speed to change with time
   }
   show(){ // show to generate tile
     push();
+
       translate(this.x, this.y);
       rectMode(CENTER); //set mode to center
+      noStroke();
+      fill("cyan");
       rect(this.w/2, this.h/2,this.w,this.h);
     pop();
+  }
+  move(){ // this is what allows tiles to move
+    this.y +=this.speed;
+  }
+  arrive(){ // returns true once tile is at its own height
+    return this.y > -this.h + 5;
+  }
+
+  missed(){ // first tile goes outside the canvas
+    return this.y > 9 * this.h/2;
+  }
+
+  touched(x,y){
+    return(x>this.x)&&(x<this.x + this.w) && (y > this.y) && (y<this.y +this.h);
   }
 
 }
 
 
 tiles = []; // store tiles
+
+function preload(){
+  soundFormats('mp3','ogg');
+  song = loadSound("coolmusic.mp3");
+
+}
+
 function setup(){
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(800,800);
   w = width;
   h = height;
-  tiles.push(new Tile(1))
+  tiles.push(new Tile(int(random(4))))
+  song.play();
 }
 
 
@@ -36,7 +67,6 @@ function setup(){
 function draw(){
 
   frameRate(30); // 30 frames per second
-
   background(0,200);
   stroke(255);
   //draw lines to split the tiles
@@ -47,5 +77,40 @@ function draw(){
   //horizontal line
   line(0,4*h/5,w,4*h/5);
 
-  tiles[0].show();
+  if(tiles[tiles.length-1].arrive()){
+    tiles.push(new Tile(int(random(4))))
+
+  }
+  if(tiles[0].missed()){
+    gameOver = true;
+  }
+
+  for(var tile of tiles){
+    if(!gameOver){
+      tile.move();
+    }
+    tile.show();
+  }
+
+  textSize(28);
+  textAlign(CENTER);
+  fill(255);
+  stroke(0);
+  text(s,w/2,32);
+  text(score,w/2,64);
+  if(gameOver){
+    background(0);
+    text('Game Over', w/2, h/2);
+    text('Score: ' +score, w/2, h/2+50);
+  }
+}
+
+function mousePressed(){//mouse pressed
+  if(tiles[0].touched(mouseX,mouseY)){
+    score+=2;
+    tiles.splice(0,1);
+  }else{
+    gameOver = true;
+  }
+
 }
